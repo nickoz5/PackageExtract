9148ec45c0f3ce30420cb89d1ad9cbe4bd865d6c
Nick Hynes <nhynes@outlook.com.au>
2017-06-09 22:05:18 +1000

    Adding support script files

---
+++ extracter_transmission.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#echo version: $TR_APP_VERSION >>/root/started
+#echo time: $TR_TIME_LOCALTIME >>/root/started
+#echo dir: "$TR_TORRENT_DIR" >>/root/started
+#echo hash: "$TR_TORRENT_HASH" >>/root/started
+#echo id: $TR_TORRENT_ID >> /root/started
+#echo name: $TR_TORRENT_NAME >> /root/started
+
+/opt/sbin/extracter -p "$TR_TORRENT_DIR/$TR_TORRENT_NAME"
+
