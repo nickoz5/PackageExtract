5ea404d02a442806d8d6a07780bc1b4f72165e53
Nick Hynes <nhynes@outlook.com.au>
2017-06-25 09:49:04 +1000

    fixes for synology install

---
+++ SynoBuildConf/INFO.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /pkgscripts/include/pkg_util.sh
+
+package="pkgext"
+version="1.2.0229"
+displayname="PackageExtracter"
+maintainer="Nick Hynes (nhynes@outlook.com.au)"
+arch="$(pkg_get_unified_platform)"
+description="Package extraction tool"
+[ "$(caller)" != "0 NULL" ] && return 0
+pkg_dump_info
+
