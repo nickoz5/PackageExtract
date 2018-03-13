5d7da51be12baf1069ab3ddb18889f9bc243b9b4
Nick Hynes <nhynes@outlook.com.au>
2018-03-13 20:20:40 +1100

    fixed logging output and default extraction script variables

--- scripts/pkgext_transmission.sh
+++ scripts/pkgext_transmission.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
+# This script is executed as a post-download step by DownloadStation to process the package.
 
-INSTALLDIR=/usr/bin/
+# Specifies where the PkgExtract application was installed.
+INSTALLDIR=/volume2/@appstore/pkgext/usr/bin/
 
-## The following additional environment variables are populated by transmission:
-## $TR_APP_VERSION
-## $TR_TIME_LOCALTIME
-## $TR_TORRENT_DIR
-## $TR_TORRENT_HASH
-## $TR_TORRENT_ID
-## $TR_TORRENT_NAME
+# Specifies the destination for TV series to be copied to
+export PKGEXT_PATH_TV=/volume1/TV\ Shows
 
-export PKGEXT_PATH_TV=/path/to/tv/series/library
-export PKGEXT_PATH_MOVIES=/path/to/movies/library
+# Specifies the destination for movies to be copied to
+export PKGEXT_PATH_MOVIES=/volume1/Movies
+
+# If specified, outputs all information to the specified log file.
+export PKGEXT_LOG=/var/log/pkgext.log
+
+# Specifies a path for temporary files to be placed during extraction of archives.
+# If this is not specified, extraction occurs in a sub-folder of the archive.
 #export PKGEXT_TEMP=/tmp/pkgext
 
-$(INSTALLDIR)pkgext -p "$TR_TORRENT_DIR/$TR_TORRENT_NAME"
+
+
+# The following additional environment variables are populated by transmission:
+#$TR_APP_VERSION
+#$TR_TIME_LOCALTIME
+#$TR_TORRENT_DIR
+#$TR_TORRENT_HASH
+#$TR_TORRENT_ID
+#$TR_TORRENT_NAME
+
+${INSTALLDIR}pkgext -p "$TR_TORRENT_DIR/$TR_TORRENT_NAME"
